259b9626dae4ec44d7ec2a06e7c5a4e7376fa219
VasylDmitrovich <Rapidvasyaalister@gmail.com>
2020-05-19 22:40:06 +0300

    Edit init_db.sql add on delete cascade

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -3,37 +3,38 @@ create database
 DEFAULT CHARACTER SET utf8
         DEFAULT COLLATE utf8_general_ci;
 
-create table if not exists products
+create table if not exists solutions.products
 (
-    id_category int auto_increment,
-    name varchar(30) not null,
-    price double not null,
-    manufacturer varchar(60) not null,
-    year_of_manufacturer int not null,
-    category varchar(30) not null,
-    constraint products_pk
-        primary key (id_category)
+    id                   int auto_increment
+        primary key,
+    name                 varchar(30) not null,
+    price                double      not null,
+    manufacturer         varchar(60) not null,
+    year_of_manufacturer int         not null,
+    category             varchar(30) not null
 );
 
-create table if not exists televisions
+create table if not exists solutions.phones
 (
-    id_category int not null,
+    id              int auto_increment
+        primary key,
     screen_diagonal double not null,
-    constraint televisions_pk
-        primary key (id_category),
-    constraint television_products_id_category_fk
-        foreign key (id_category) references products (id_category)
-);
-
-create table if not exists phones
-(
-    id_category int not null,
-    screen_diagonal double not null,
-    ram double not null,
+    ram             double not null,
     internal_memory double not null,
-    constraint phones_pk
-        primary key (id_category),
-    constraint phones_products_id_category_fk
-        foreign key (id_category) references products (id_category)
+    id_category     int    not null,
+    constraint phones_products_id_fk
+        foreign key (id_category) references products (id)
+            on delete cascade
+);
+
+create table if not exists solutions.televisions
+(
+    id              int auto_increment
+        primary key,
+    screen_diagonal double not null,
+    id_category     int    not null,
+    constraint televisions_products_id_fk
+        foreign key (id_category) references products (id)
+            on delete cascade
 );
 
