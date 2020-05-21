1bae3f1f6f21e7817fb1e5057327b49113271083
VasylDmitrovich <Rapidvasyaalister@gmail.com>
2020-05-21 19:35:07 +0300

    add simple addProduct on page

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
+    $("#formAddProduct").click(function() {
+        $("#formProduct").toggle();
+    });
+});
+
+$(document).ready(function() {
     $("#formAddPhone").click(function() {
         $("#formPhone").toggle();
     });
 });
+
 $(document).ready(function() {
     $("#formAddTelevision").click(function() {
         $("#formTelevision").toggle();
